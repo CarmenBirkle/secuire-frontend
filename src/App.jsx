7aa837308b8952bb4d8cb9631398b9d5aa832a8c
Carolin Walter <carolinwalter@gmx.net>
2023-05-11 05:47:29 +0200

    add fonts and css styles

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,21 @@ import ForgotPassword from './pages/ForgotPassword';
 import Login from './pages/Login';
 import Error from './pages/Error';
 import LegalNotice from './pages/LegalNotice';
+import './fonts/poppins-v20-latin-700.eot';
+import './fonts/poppins-v20-latin-700.svg';
+import './fonts/poppins-v20-latin-700.ttf';
+import './fonts/poppins-v20-latin-700.woff';
+import './fonts/poppins-v20-latin-700.woff2';
+import './fonts/poppins-v20-latin-900.eot';
+import './fonts/poppins-v20-latin-900.svg';
+import './fonts/poppins-v20-latin-900.ttf';
+import './fonts/poppins-v20-latin-900.woff';
+import './fonts/poppins-v20-latin-900.woff2';
+import './fonts/poppins-v20-latin-regular.eot';
+import './fonts/poppins-v20-latin-regular.svg';
+import './fonts/poppins-v20-latin-regular.ttf';
+import './fonts/poppins-v20-latin-regular.woff';
+import './fonts/poppins-v20-latin-regular.woff2';
 
 const App = () => {
   return (
