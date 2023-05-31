189f788da9f267a8ae487283e232faa0523c417a
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-31 06:04:28 +0200

    solve warning bcrypt package

--- src/components/SingleDataEntryDetail.jsx
+++ src/components/SingleDataEntryDetail.jsx
@@ -14,6 +14,7 @@ const SingleDataEntryDetail = ({
   setEditMode
 }) => {
   const { t } = useTranslation(['main']);
+  
   const renderCustomTopics = () => {
     if (dataEntry.customTopics.length !== 0) {
       return dataEntry.customTopics.map((topic, index) => (
