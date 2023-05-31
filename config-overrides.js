c26ba7ffd9da521cffc713a1111447514de7f629
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-31 06:15:21 +0200

    solve buffer error

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
 module.exports = function override(config, env) {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    crypto: require.resolve('crypto-browserify'),
-            stream: require.resolve("stream-browserify") 
-  };
+    config.resolve.fallback = {
+        ...config.resolve.fallback, // die vorherigen Fallbacks beibehalten
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/') // neuer Eintrag
+    };
 
-  return config;
+    return config;
 };
