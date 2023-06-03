4523f096d96314eb5a89776f6a4586ef55fe8f54
Schlabs, Dominik <dominik.schlabs@iubh-fernstudium.de>
2023-06-03 16:41:54 +0000

    Updated copyIISConfig.js

--- iisConfig/copyIISConfig.js
+++ iisConfig/copyIISConfig.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 
-const webConfigPath = './dist/web.config';
+const webConfigPath = './build/web.config';
 
 if (fs.existsSync(webConfigPath)) {
     fs.unlinkSync(webConfigPath);
