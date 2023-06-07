07d61c547f38690c6e4b95a6231f0efb9f50620f
Carolin Walter <carolinwalter@gmx.net>
2023-06-07 22:03:35 +0200

    Style nice2know

--- src/pages/Nice2Know.jsx
+++ src/pages/Nice2Know.jsx
@@ -5,61 +5,76 @@ const Nice2Know = () => {
 
   return (
     <>
-      <h2>{t('nice2know')}</h2>
-      <h4>{t('title_content1')}</h4>
-      <ol>
-        <li>
-          <h5>{t('content1_tip1')}</h5>
-          {t('content1_tip1_c')}
-        </li>
-        <li>
-          <h5>{t('content1_tip2')}</h5>
-          {t('content1_tip2_c')}
-        </li>
-        <li>
-          <h5>{t('content1_tip3')}</h5>
-          {t('content1_tip3_c')}
-        </li>
-        <li>
-          <h5>{t('content1_tip4')}</h5>
-        </li>
-        <li>
-          <h5>{t('content1_tip5')}</h5>
-        </li>
-        <li>
-          <h5>{t('content1_tip6')}</h5>
-          {t('content1_tip6_c')}
-        </li>
-      </ol>
-
-      <div>
-        {t('Source')}:{' '}
-        <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/cyber-sicherheitsempfehlungen_node.html">
-          BSI - Bundesamt für Sicherheit in der Informationstechnik
-        </a>
+      <h1>{t('nice2know')}</h1>
+      
+      <section className='safeInternet'>
+        <h2>{t('title_content1')}</h2>
+        <ol>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip1')}</h3>
+            <p>{t('content1_tip1_c')}</p>
+          </li>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip2')}</h3>
+            <p>{t('content1_tip2_c')}</p>
+          </li>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip3')}</h3>
+            <p>{t('content1_tip3_c')}</p>
+          </li>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip4')}</h3>
+          </li>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip5')}</h3>
+          </li>
+          <li>
+            <h3 className='subheadline'>{t('content1_tip6')}</h3>
+            <p>{t('content1_tip6_c')}</p>
+          </li>
+        </ol>
+        <div className='distanceBottom'>
+          {t('Source')}:{' '}
+          <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/cyber-sicherheitsempfehlungen_node.html">
+            BSI - Bundesamt für Sicherheit in der Informationstechnik
+          </a>
+        </div>
+      </section>
+      
+      <section className='safeInternet'>
+        <h2>{t('title_content2')}</h2>
+        <p>{t('content2')}</p>
+        <div className='distanceBottom'>
+          {t('Source')}:{' '}
+          <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/Accountschutz/Sichere-Passwoerter-erstellen/sichere-passwoerter-erstellen_node.html">
+            BSI - Bundesamt für Sicherheit in der Informationstechnik
+          </a>
       </div>
+      </section >
 
-      <h4>{t('title_content2')}</h4>
-      <p>{t('content2')}</p>
+      <section className='safeInternet'>
+        <h2>{t('title_content3')}</h2>
+        <p>{t('content3a')}</p>
+        
+        <p><a href="https://sec.hpi.de/ilc/"> https://sec.hpi.de/ilc/</a> {t('content3b')}</p>
+        <div>
+          {t('Source')}:{' '}
+          <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/Accountschutz/Sichere-Passwoerter-erstellen/Umgang-mit-Passwoertern/umgang-mit-passwoertern_node.html">
+            BSI - Bundesamt für Sicherheit in der Informationstechnik
+          </a>
+        </div>
+      </section>
 
-      <div>
-        {t('Source')}:{' '}
-        <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/Accountschutz/Sichere-Passwoerter-erstellen/sichere-passwoerter-erstellen_node.html">
-          BSI - Bundesamt für Sicherheit in der Informationstechnik
-        </a>
-      </div>
+      
+      
 
-      <h4>{t('title_content3')}</h4>
-      <p>{t('content3a')}</p>
-      <a href="https://sec.hpi.de/ilc/"> https://sec.hpi.de/ilc/</a>
-      <p>{t('content3b')}</p>
+      
 
-      <div>
-        {t('Source')}:{' '}
-        <a href="https://www.bsi.bund.de/DE/Themen/Verbraucherinnen-und-Verbraucher/Informationen-und-Empfehlungen/Cyber-Sicherheitsempfehlungen/Accountschutz/Sichere-Passwoerter-erstellen/Umgang-mit-Passwoertern/umgang-mit-passwoertern_node.html">
-          BSI - Bundesamt für Sicherheit in der Informationstechnik
-        </a>
-      </div>
+      
+      
+      
+
+      
     </>
   );
 };
