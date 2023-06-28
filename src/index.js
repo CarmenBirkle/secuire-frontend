1f871b208246f814f21ddfddfc50a35127b66264
Carolin Walter <carolinwalter@gmx.net>
2023-06-28 19:09:55 +0200

    fix bug 'downloadable font'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './fonts/poppins-v20-latin-regular.eot'; 
+import './fonts/poppins-v20-latin-regular.eot?#iefix';
+import './fonts/poppins-v20-latin-regular.woff2'; 
+import './fonts/poppins-v20-latin-regular.woff'; 
+import './fonts/poppins-v20-latin-regular.ttf'; 
+import './fonts/poppins-v20-latin-regular.svg#Poppins';
+import './fonts/poppins-v20-latin-700.eot'; 
+import './fonts/poppins-v20-latin-700.eot?#iefix';
+import './fonts/poppins-v20-latin-700.woff2'; 
+import './fonts/poppins-v20-latin-700.woff'; 
+import './fonts/poppins-v20-latin-700.ttf'; 
+import './fonts/poppins-v20-latin-700.svg#Poppins';
+import './fonts/poppins-v20-latin-900.eot'; 
+import './fonts/poppins-v20-latin-900.eot?#iefix';
+import './fonts/poppins-v20-latin-900.woff2'; 
+import './fonts/poppins-v20-latin-900.woff'; 
+import './fonts/poppins-v20-latin-900.ttf'; 
+import './fonts/poppins-v20-latin-900.svg#Poppins';
+
 import './i18n';
 import { AppProvider } from './components/helperSites/AppContext';
 
