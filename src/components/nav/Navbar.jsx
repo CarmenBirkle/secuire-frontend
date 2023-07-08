3434241d8e2c2a3649b127b25977dc480ffb666c
Carmen Birkle <carmen.birkle@iu-study.org>
2023-07-08 06:30:25 +0200

    clean code components/nav with comments

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,5 +1,10 @@
-import React, { useContext} from 'react';
-import { useState } from "react";
+/**
+* @fileOverview
+* Navbar component for rendering a navigation bar with links.
+* Provides navigation links based on user login status and language translation.
+*/
+
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { AppContext } from '../helperSites/AppContext';
@@ -7,25 +12,23 @@ import { AppContext } from '../helperSites/AppContext';
 const Navbar = () => {
   const { t } = useTranslation(['sidebar']);
 
-  
-  const navbutton = React.useRef(document.getElementById("mobile_button"));
-  const navigation = React.useRef(document.getElementById("nav"));
+  const navbutton = React.useRef(document.getElementById('mobile_button'));
+  const navigation = React.useRef(document.getElementById('nav'));
   const { setShouldRenderCreateDataEntry } = useContext(AppContext);
   const { setCalledFromNavbar, logIn } = useContext(AppContext);
   const headerElement = document.getElementById('header');
-  
-  const navClick = event => {
-    const links = document.querySelectorAll(".navlink"); 
+
+  const navClick = (event) => {
+    const links = document.querySelectorAll('.navlink');
     Array.from(links).forEach((el) => {
       el.classList.remove('active');
     });
-    event.currentTarget.classList.add('active'); 
-    navbutton.current.classList.replace('open', 'closed');   
+    event.currentTarget.classList.add('active');
+    navbutton.current.classList.replace('open', 'closed');
     navigation.current.classList.remove('open');
     headerElement.parentElement.classList.remove('front');
     setShouldRenderCreateDataEntry(false);
   };
-  
 
   return (
     <ul>
